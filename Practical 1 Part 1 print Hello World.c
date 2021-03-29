07383d2f0b702482e77db27e725cbd3232c3fd3b
SRutwik <81549334+SRutwik@users.noreply.github.com>
2021-03-29 12:12:21 +0530

    First Commit
    First Hello World program in C Programming
    Pre-Requisites:C Programming

---
+++ Practical 1 Part 1 print Hello World.c
@@ -0,0 +1,16 @@
+/*
+  Name: Rutwik Senta
+  Roll No.:208
+  Batch-B1
+  Div-B
+*/
+/*Practical 1 Part 1 print "Hello World"
+  Date:07/02/2021*/
+#include<stdio.h>
+
+int main()
+
+{
+    printf("Hello World");
+    return 0;
+}
